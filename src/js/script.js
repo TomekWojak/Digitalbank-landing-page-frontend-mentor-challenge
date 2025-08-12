df02ba1110dc31a549101b766ba1052fd8d034d6
Tomek <tomek.wojak796@gmail.com>
2025-08-12 20:05:00 +0200

    initially finished header

--- src/js/script.js
+++ src/js/script.js
@@ -3,25 +3,42 @@ document.addEventListener("DOMContentLoaded", function () {
 	const navMobile = document.querySelector(".nav__mobile");
 	const allMobileLi = document.querySelectorAll(".nav__mobile-li");
 	const overlay = document.querySelector(".overlay");
+	const allDesktopLinks = document.querySelectorAll(".nav__desktop-link");
 
+	const showOverlay = () => {
+		overlay.classList.add("active");
+	};
+	const hideOverlay = () => {
+		setTimeout(() => {
+			overlay.classList.remove("active");
+		}, 300);
+	};
 	const handleNav = () => {
 		const isOpened = navToggler.getAttribute("aria-expanded");
 
 		if (isOpened === "false") {
 			showNav();
-			navToggler.classList.add("active");
-			navToggler.setAttribute("aria-expanded", "true");
-			navToggler.setAttribute("aria-label", "Close mobile navigation");
+			togglerOn();
+			showOverlay();
 		} else {
 			showNav();
-			navToggler.classList.remove("active");
-			navToggler.classList.add("unactive");
-			navToggler.setAttribute("aria-expanded", "false");
-			navToggler.setAttribute("aria-label", "Show mobile navigation");
-			setTimeout(() => {
-				navToggler.classList.remove("unactive");
-			}, 400);
+			togglerOff();
+			hideOverlay();
 		}
+	};
+	const togglerOff = () => {
+		navToggler.classList.remove("active");
+		navToggler.classList.add("unactive");
+		navToggler.setAttribute("aria-expanded", "false");
+		navToggler.setAttribute("aria-label", "Show mobile navigation");
+		setTimeout(() => {
+			navToggler.classList.remove("unactive");
+		}, 400);
+	};
+	const togglerOn = () => {
+		navToggler.classList.add("active");
+		navToggler.setAttribute("aria-expanded", "true");
+		navToggler.setAttribute("aria-label", "Close mobile navigation");
 	};
 
 	const showNav = () => {
@@ -46,5 +63,20 @@ document.addEventListener("DOMContentLoaded", function () {
 			navMobile.addEventListener("transitionend", removeActive);
 		}
 	};
+	const removeActive = () => {
+		allDesktopLinks.forEach((link) => link.classList.remove("active"));
+	};
+	allDesktopLinks.forEach((link) =>
+		link.addEventListener("click", (e) => {
+			e.preventDefault();
+			removeActive();
+			link.classList.add("active");
+		})
+	);
+	overlay.addEventListener("click", () => {
+		showNav();
+		togglerOff();
+		hideOverlay();
+	});
 	navToggler.addEventListener("click", handleNav);
 });
